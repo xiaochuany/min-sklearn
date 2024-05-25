5bedad0820354bc7351a2d47da0ee3c773893a63
xiaochuany <xiaochuan.j.yang@gmail.com>
2024-05-26 00:05:19 +0200

    update

--- min_sklearn/_modidx.py
+++ min_sklearn/_modidx.py
@@ -5,4 +5,9 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://xiaochuany.github.io',
                 'git_url': 'https://github.com/xiaochuany/min-sklearn',
                 'lib_path': 'min_sklearn'},
-  'syms': {'min_sklearn.core': {'min_sklearn.core.foo': ('core.html#foo', 'min_sklearn/core.py')}}}
+  'syms': { 'min_sklearn.metrics': { 'min_sklearn.metrics.accuracy_score': ('metrics.html#accuracy_score', 'min_sklearn/metrics.py'),
+                                     'min_sklearn.metrics.f1_score': ('metrics.html#f1_score', 'min_sklearn/metrics.py'),
+                                     'min_sklearn.metrics.precision_recall_fscore': ( 'metrics.html#precision_recall_fscore',
+                                                                                      'min_sklearn/metrics.py'),
+                                     'min_sklearn.metrics.precision_score': ('metrics.html#precision_score', 'min_sklearn/metrics.py'),
+                                     'min_sklearn.metrics.recall_score': ('metrics.html#recall_score', 'min_sklearn/metrics.py')}}}
